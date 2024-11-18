b4f864c700c82274299274ed5e0ece98d59a69f4
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-18 18:46:08 +0100

    Removed anything non explicitly asked

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,13 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') throw new Error('TypeError: floors must be a number');
     this._floors = floors; // eslint-disable-line no-underscore-dangle
-  }
-
-  // Getter for sqft
-  get sqft() {
-    return this._sqft; // eslint-disable-line no-underscore-dangle
   }
 
   // Getter for floors
