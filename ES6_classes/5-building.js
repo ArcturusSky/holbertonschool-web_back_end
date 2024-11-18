b83b2043525ca35897cff0f224694ea651cfb545
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-18 17:53:04 +0100

    Finally, the issue was to check if the the constructeur was in an object which WASN'T a Building (so not direct instantiation) AND lacking the correct error.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new Error('TypeError: sqft must be a number');
+    // Check if NOT an instance of Building itself AND doesn't overwrite the required method
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
+    if (typeof sqft !== 'number') throw new Error('TypeError: sqft must be a number');
     this._sqft = sqft; // eslint-disable-line no-underscore-dangle
   }
 
@@ -17,9 +19,5 @@ export default class Building {
       throw new Error('TypeError: sqft must be a string');
     }
     this._sqft = newsqft; // eslint-disable-line no-underscore-dangle
-  }
-
-  evacuationWarningMessage() { // eslint-disable-line class-methods-use-this
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
