4e8aace69ac6b06f3c87d6ba9e37a8216d75eb87
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-18 19:04:48 +0100

    I pushed back MY code, because I saw the freaking issue. It was just that I added a dot at the end of my sentence. dot that wasn't necessary.

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,18 +3,21 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    if (typeof floors !== 'number') throw new Error('TypeError: floors must be a number');
+    this._floors = floors; // eslint-disable-line no-underscore-dangle
   }
 
+  // Getter for sqft
   get sqft() {
-    return this._sqft;
+    return this._sqft; // eslint-disable-line no-underscore-dangle
   }
 
+  // Getter for floors
   get floors() {
-    return this._floors;
+    return this._floors; // eslint-disable-line no-underscore-dangle
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`; // eslint-disable-line no-underscore-dangle
   }
 }
