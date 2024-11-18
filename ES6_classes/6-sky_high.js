485577366207d16d4acce9020e6c3ec1a313f4df
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-18 18:59:09 +0100

    test with another code which is note mine but exactly the same

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,21 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') throw new Error('TypeError: floors must be a number');
-    this._floors = floors; // eslint-disable-line no-underscore-dangle
+    this._floors = floors;
   }
 
-  // Getter for sqft
   get sqft() {
-    return this._sqft; // eslint-disable-line no-underscore-dangle
+    return this._sqft;
   }
 
-  // Getter for floors
   get floors() {
-    return this._floors; // eslint-disable-line no-underscore-dangle
+    return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`; // eslint-disable-line no-underscore-dangle
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
