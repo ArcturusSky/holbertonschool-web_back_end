bc3a80e2ab7a52673418bb4ed210f1fffc911a6f
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-18 19:31:05 +0100

    typo fix

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,16 +1,17 @@
 export default class Airport {
-    constructor(name, code) {
-        // Check if all attribute are what they are supposed to be
-        if (typeof code !== 'string') {
-          throw new Error('TypeError: Code must be a string');
-        }
-        if (typeof name !== 'string') {
-          throw new Error('TypeError: Name must be a string');
-        }
-        this._code = code; // eslint-disable-line no-underscore-dangle
-        this._name = name; // eslint-disable-line no-underscore-dangle
-      }
-    toString() {
-        return `object ${this._code}`;
+  constructor(name, code) {
+    // Check if all attribute are what they are supposed to be
+    if (typeof code !== 'string') {
+      throw new Error('TypeError: Code must be a string');
     }
+    if (typeof name !== 'string') {
+      throw new Error('TypeError: Name must be a string');
+    }
+    this._code = code; // eslint-disable-line no-underscore-dangle
+    this._name = name; // eslint-disable-line no-underscore-dangle
+  }
+
+  toString() {
+    return `[object ${this._code}]`; // eslint-disable-line no-underscore-dangle
+  }
 }
