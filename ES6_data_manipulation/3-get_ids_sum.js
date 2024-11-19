a70e1033609d7412d618a6378b0e9fadbb14443d
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-19 15:59:44 +0100

    [FEAT] Add getListStudentIds function
    - Create function to sum student IDs
    - Use reduce method to calculate total of all student IDs
    - Return sum as a single number

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+export default function getListStudentIds(StudentsList) { // eslint-disable-line no-unused-vars
+  const SumIds = StudentsList.reduce((total, student) => total + student.id, 0);
+  return SumIds
+}
