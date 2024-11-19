f931e535a933afa162d6e6729604cbce16f10a92
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-19 19:39:21 +0100

    lint changed

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(StudentsList) { // eslint-disable-line no-unused-vars
   const SumIds = StudentsList.reduce((total, student) => total + student.id, 0);
-  return SumIds
+  return SumIds;
 }
