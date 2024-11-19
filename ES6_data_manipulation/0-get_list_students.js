5e62a63e1e8ccb48da672cd3695913788160d117
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-19 14:31:27 +0100

    [FEAT] Add getListStudents function
    - Create function to return a list of student objects
    - Include three hardcoded student entries
    - Each student has firstname, id, and location properties

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+function getListStudents() { // eslint-disable-line no-unused-vars
+  const StudentList = [];
+
+  const student1 = {
+    firstname: 'Guillaume',
+    id: 1,
+    location: 'San Francisco',
+  };
+  const student2 = {
+    firstname: 'James',
+    id: 2,
+    location: 'Columbia',
+  };
+  const student3 = {
+    firstname: 'Serena',
+    id: 5,
+    location: 'San Francisco',
+  };
+  StudentList.push(student1, student2, student3);
+  return StudentList;
+}
