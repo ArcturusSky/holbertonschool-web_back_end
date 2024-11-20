d56f8af379699d70a94799d38a46c90954fbdd23
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-20 18:48:39 +0100

    Nope still  no understood

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
-  // create the array buffer of 8 bytes
-  const buffer = new ArrayBuffer(length);
 
-  // Create a 8 bit signes integer view on the buffer
-  const int8View = new Int8Array(buffer);
+    // create the array buffer of 8 bytes
+    const buffer = new ArrayBuffer(length);
 
-  // Set values in the type array
-  int8View[position] = value;
-  if (!int8View[position] === value) throw Error('Position outside range');
-  console.lof(int8View);
+    // Create a 8 bit signes integer view on the buffer
+    const int8View = new Int8Array(buffer);
+
+    // Set values in the type array
+    int8View[position] = value;
+    if (!int8View[position] === value)
+        throw Error('Position outside range')
 }
