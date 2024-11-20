d9786ff073c198e437de84639ef0e6e38ab58278
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-20 18:36:33 +0100

    Trying to fix, return direct the array instead of creating an array

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,4 @@
 export default function
 getStudentsByLocation(StudentsList, city) { // eslint-disable-line no-unused-vars
-  const StudentByLocation = StudentsList.filter((student) => student.location === city);
-  return StudentByLocation;
+  return StudentsList.filter((student) => student.location === city);
 }
