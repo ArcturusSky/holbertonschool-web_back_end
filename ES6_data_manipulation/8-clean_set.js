9c91a96a845d7c3eda4a58201988281e374182e0
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-20 19:17:19 +0100

    Output is correct but I'm sure it's not the right way to do it.

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,16 @@
-export default function cleanSet(set, startString){
-    
+export default function cleanSet(set, startString) {
+  // Convert into an array to allow modification
+
+  if (startString === '') return '';
+
+  const ArrayToModify = Array.from(set);
+
+  // Filter words who start with "startstring"
+  // Map the words and cut the first part
+  const FilteredArray = ArrayToModify.filter((words) => words.startsWith(startString));
+  const ArrayWithoutPrefix = FilteredArray.map((words) => words.slice(startString.length));
+
+  // Convert the array into a string and join each word of the array by "-"
+  const resultString = ArrayWithoutPrefix.join('-');
+  return resultString;
 }
