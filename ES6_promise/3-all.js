cda4c9ca4061851d90d88a97e06222185a665b8d
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-21 22:01:56 +0100

    I had the logic but not the right syntax. Needed Ai help to get the correct one.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  // "uploadPhoto" and "createUser" with their "()" because
+  // calling the function to have their result
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      // "photoResponse" and "userReponse" are name given
+      // promises's results that I have to select the attribute I want of the response.
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
