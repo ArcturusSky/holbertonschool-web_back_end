55e419276b160987168ee45f93735c3f7d5d1192
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-22 02:34:50 +0100

    I can't believe I forgot ONCE AGAIN the "return" statement before" Promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   // "uploadPhoto" and "createUser" with their "()" because
   // calling the function to have their result
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       // "photoResponse" and "userResponse" are name given
       // promises's results that I have to select the attribute I want of the response.
