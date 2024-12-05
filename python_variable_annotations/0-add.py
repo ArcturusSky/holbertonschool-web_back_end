0bf2b06674582043fb07a02ab6cea9c66467d654
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-05 11:20:13 +0100

    [FEAT] Add type-annotated function for float addition
    - Implemented a new module with a type-annotated function `add`
    - The function takes two float parameters and returns their sum as a float
    - Added docstrings for both the module and the function to explain their purpose
    - Ensures type safety and improves code readability
    
    This addition provides a simple, type-safe way to add two float numbers, which can be useful in various parts of the application where precise float arithmetic is needed.

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+"""
+Module to type_annotated a function add to takes two float and return
+their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Function to add 2 floats and return their sum as a float too"""
+    return (a + b)
