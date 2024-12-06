5d06a29a16a50e89704df923cd137208a1a4eb84
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-06 23:40:32 +0100

    [FEAT] Add function to create asyncio.Task from wait_random
    - Created a module to define a function for creating an asyncio.Task
    - Implemented `task_wait_random` function that:
      - Takes `max_delay` as an argument
      - Creates and returns an asyncio.Task that runs `wait_random` with the specified delay
    - Imported `wait_random` from the previous module
    
    This addition facilitates the creation of tasks for asynchronous execution using the existing `wait_random` function.

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 """
+Module to create an asyncio.Task from the wait_random function.
 """
 
 
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Function taht create an asyncio.Task that runs
+    wait_random with the given max_delay.
+    """
+
+    return asyncio.create_task(wait_random(max_delay))
