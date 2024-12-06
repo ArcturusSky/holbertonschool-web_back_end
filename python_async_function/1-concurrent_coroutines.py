efa7bbad3d7ab97797f4ea174b46ba7117a2680c
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-06 23:14:55 +0100

    [FEAT] Add concurrent execution of multiple coroutines
    - Created a module to demonstrate concurrent execution with `wait_n`
    - Implemented `wait_n` function that:
      - Takes `n` (number of coroutines) and `max_delay`
      - Spawns `wait_random` `n` times and collects delays
      - Returns the list of delays sorted in ascending order
    - Used `asyncio.gather()` for concurrent execution of coroutines
    - Imported `wait_random` from the previous module
    
    This addition showcases how to execute multiple coroutines concurrently and gather their results efficiently.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 """
+Module to illustrate several coroutine at the same times
 """
 
+import asyncio
+from typing import List
 
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Function that return the list of all the delays (as float values)
+    in ascending order
+    """
+    # List of coroutines to do
+    tasks: List[float] = [
+        wait_random(max_delay) for i in range(n)]
+
+    # Use asyncio.gather to execute all the coroutine
+    # on the same time and waiting all their results in once
+    waiting_time_list = await asyncio.gather(*tasks)
+    # Haha is that a pointer toward the list of task to do?
+
+    return sorted(waiting_time_list)
