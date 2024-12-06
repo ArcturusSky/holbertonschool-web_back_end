d8ede36c93f9b13b3f12a909797de28404afe5ee
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-06 16:50:44 +0100

    Forgot some docstring

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,4 +6,7 @@ argument and returns the string representation of the float
 
 
 def to_str(n: float) -> str:
+    """
+    Function that returns the string representation of a float
+    """
     return str(n)
