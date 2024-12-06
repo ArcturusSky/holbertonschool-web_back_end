fe68a352008c6d02fcf9d3755b29f20f5034d8c2
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-06 23:57:52 +0100

    pycode

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -6,8 +6,13 @@ Module to augment the code provided
 
 from typing import Sequence, Any, Optional
 
+
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    NO TIME this is documenteeed ebrgfbsvj
+    erfdc
+    """
     if lst:
         return lst[0]
     else:
