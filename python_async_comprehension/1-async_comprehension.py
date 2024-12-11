4f119ee1bf23639980a0f7a2445c170cf4d8eece
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 13:25:13 +0100

    [REFACTOR] Add return type annotation to async_comprehension
    - Added return type annotation `-> List[float]` to `async_comprehension` function
    - Imported `List` from `typing` module
    
    This update improves type hinting, making the function's return type explicit and enhancing code readability and type safety.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 Module to illustrate synx comprehension
 """
 
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     This function create a list with the 10 values inside
     """
