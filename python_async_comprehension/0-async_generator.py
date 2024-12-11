05d6c7486f271d4f555b7def2d5b0ca8629a64a8
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 13:17:29 +0100

    translated comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -13,6 +13,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Asynchronous generator that loops 10 times, waits for 1 second
     in each iteration, and yields a random float between 0 and 10.
     """
-    for _ in range(10):  # Utilisation de _ au lieu de i si non utilisé
+    for _ in range(10):  # Using _ instead of "i" since not used
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
