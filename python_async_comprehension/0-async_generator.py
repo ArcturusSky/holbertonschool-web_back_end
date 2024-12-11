90b35d5c2e1016e06844d6e4438f29990cb74a7c
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 11:33:16 +0100

    [FEAT] Implement basic async generator
    - Created a module with an async generator function `async_generator`
    - The function:
      - Loops 10 times
      - Asynchronously waits 1 second in each iteration
      - Yields a random float between 0 and 10
    - Used `AsyncGenerator[int, None]` type hint for proper typing
    - Utilized `asyncio.sleep()` for asynchronous waiting
    - Employed `random.uniform()` for generating random floats
    
    Personnal note: I used already the type hiting for it, the output is conforme to what is awaited by the checker but I'm not sure that it's exactly what they will want.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,15 @@ Module to illustrate a basic async generator
 
 
 import random
+from typing import AsyncGenerator
+import asyncio
 
-async def async_generator():
-    """
-    Basic async_generator
-    """
 
+async def async_generator() -> AsyncGenerator[int, None]:
+    """
+    Basic coroutine that will loop 10 times, each time asynchronously
+    wait 1 seconed
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
