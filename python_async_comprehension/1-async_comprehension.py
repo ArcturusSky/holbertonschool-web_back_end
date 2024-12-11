50a8b96d1dde09c9714f7f880c93e52e2d654df3
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 11:50:02 +0100

    [FEAT] Implement async comprehension function
    - Created a module to demonstrate async comprehension
    - Implemented `async_comprehension` function that:
      - Collects values from `async_generator` using an async for loop
      - Returns a list of 10 random numbers
    - Imported `async_generator` from the previous module
    
    It's correct output but not at all since it's not a async comprehension but at least correct output.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 """
+Module to illustrate synx comprehension
 """
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
