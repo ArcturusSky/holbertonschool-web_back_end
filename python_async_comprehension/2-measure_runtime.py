2b6892768c9090ad1828135e65c5ee1443436573
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 13:28:46 +0100

    [REFACTOR] Enhance measure_runtime function with type annotation
    - Added return type annotation `-> float` to `measure_runtime` function
    - Removed unused `asyncio` import
    
    This update improves type hinting and removes unnecessary imports, enhancing code clarity and maintainability.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,11 +6,10 @@ previous async generator function 4 times
 
 
 import time
-import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Function to measure the runtime
     """
