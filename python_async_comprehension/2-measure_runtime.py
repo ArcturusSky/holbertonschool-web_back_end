5fa62a48224dbc33234b0bead1c47678fe6b991e
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 12:13:26 +0100

    [FEAT] Implement runtime measurement for async comprehension
    - Created a module to measure the runtime of async comprehension
    - Implemented `measure_runtime` function that:
      - Records start time
      - Executes `async_comprehension`
      - Records end time
      - Returns the total runtime
    - Used `time.time()` for precise time measurement
    - Imported `async_comprehension` from the previous module
    
    Personnal note: I used a previous task where I had to measure the runtime, it's quite simple. Now just see if the checker agrees

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 """
+Module to measure the runtime of the
+previous async generator function 4 times
 """
+
+
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Function to measure the runtime
+    """
+
+    start_time = time.time()
+    await async_comprehension()
+    end_time = time.time()
+
+    return (end_time - start_time) 
