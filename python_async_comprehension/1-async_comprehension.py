6392dbc30732601692264a466a9fd4e41d4624c5
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-11 11:57:48 +0100

    Forgot docstring.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """
+    This function create a list with the 10 values inside
+    """
     result = [value async for value in async_generator()]
     return result
