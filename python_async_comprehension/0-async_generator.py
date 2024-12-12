3df098e697c85692ef8947fd7c9218c1ce732ea3
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-12 19:32:28 +0100

    Trying returning a Generator instead of AsyncGenerator even if not logical

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Module to illustrate a basic async generator
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that loops 10 times, waits for 1 second
     in each iteration, and yields a random float between 0 and 10.
