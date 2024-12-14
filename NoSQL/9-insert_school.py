a3d40750b0d891ecbe2c3232c9af6bdbcd382cce
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-12-14 16:13:39 +0100

    Python script to add a document in a given collection. Since mongodb 4.4 for the checker I used "insert()" and not "insert_one()" as it should be.

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to illustrate mongodb use with Python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert a new document in the collection
+    based on kwargs
+    """
+
+    documents = mongo_collection.insert(kwargs)
+
+    return documents.inserted_id
